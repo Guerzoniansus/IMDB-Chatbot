9f505404e596c27faf7017edfefa62364d2ba801
Guerzoniansus <weezimonkey@gmail.com>
2021-04-02 20:56:07 +0200

    Finished chatbot

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,7 @@
 import { QUESTIONS} from "./questions.js";
 
 const DB_URL = "http://localhost:8080";
+const IMAGE_PATH = "images/";
 
 const INPUT_FIELD = document.getElementById("question-input");
 const MESSAGE_BOX = document.getElementById("message-container");
@@ -8,31 +9,85 @@ const MESSAGE_BOX = document.getElementById("message-container");
 const QUESTION_DISPLAY = document.getElementById("question-display");
 const ANSWER_OUTPUT = document.getElementById("output-answer-container");
 
+/**
+ * Format for text answers
+ * @param text The text to display
+ * @returns {string} A formatted HTML element with the text ready to add on the page
+ * @constructor
+ */
 const ANSWER_TEXT_FORMAT = (text) => `<p class="answer-text">${text}</p>`;
+
+/**
+ * Format for image answers
+ * @param path The image name
+ * @returns {string} A formatted HTML element with the image in it ready to be added to the page
+ */
 const ANSWER_IMAGE_FORMAT = (path) => `<a target="_blank" href="${path}"><img class="answer-image" src="${path}"></a>`;
+
+/**
+ * A format for clickable questions in the chat box
+ * @param question The question
+ * @returns {string} A formatted HTML element for a clickable question
+ */
+const QUESTION_FORMAT = (question) => `➡ <span class="clickable-question" onclick="clickedQuestion(this.innerText)">${question}</span>`
 
 const BOT = "IMDB-Bot";
 const USER = "Ik";
-const MESSAGE = (sender, message) => "<p class='message'>" + sender + ": " + message + "</p>";
+
+/**
+ * A format for chat message
+ * @param sender The name of the sender
+ * @param message The message that was sent
+ * @returns {string} A formatted HTML element that can be added to the web page
+ */
+const MESSAGE = (sender, message) => "<p class='message'><b>" + sender + "</b>: " + message + "</p>";
 
 const GREETING = "Hallo.";
 
 init();
 
+
+/**
+ * Initialize stuff
+ */
 function init() {
-    INPUT_FIELD.addEventListener("keyup", function(e) {
+    INPUT_FIELD.addEventListener("keyup", (e) => {
         if (e.key == "Enter") {
-            processQuestion();
+            handleInput();
         }
     });
+
+    // This is needed because this javascript is imported as a module which means that
+    // functions are not recognized in the global scope
+    // which means that the HTML won't recognize the function when defined inside the HTML itself
+    // (aka the onclick= part of the <span class=clickable-question>)
+    // and using this line here fixes that
+    window.clickedQuestion = clickedQuestion;
 }
 
-function processQuestion() {
-    // Show input in chat
-    sendMessage(USER, INPUT_FIELD.value);
+/**
+ * Click event handler for clickable questions
+ * @param question The queston that got clicked
+ */
+function clickedQuestion(question) {
+    processQuestion(question);
+}
 
-    // Get input
+/**
+ * Handle user input
+ */
+function handleInput() {
     const input = sanitizedQuestion(INPUT_FIELD.value);
+    processQuestion(input);
+}
+
+/**
+ * Handle a question
+ * @param input The input that needs to be checked for a question
+ */
+function processQuestion(input) {
+    // Show input in chat
+    sendMessage(USER, input);
     clearInputField();
 
     // Check if input is empty
@@ -71,6 +126,7 @@ function processQuestion() {
         return;
     }
 
+    // Answer the question
     const question = QUESTIONS[bestMatch];
     clearOutput();
     processAnswer(question);
@@ -78,10 +134,11 @@ function processQuestion() {
     sendMessage(BOT, "Alsjeblieft.");
 }
 
-
+/**
+ * Handle the answering process for a question
+ * @param question The question to display the answer of
+ */
 function processAnswer(question) {
-    const imagePath = "images/";
-
     QUESTION_DISPLAY.innerHTML = question.question;
 
     question.answers.forEach((answer) => {
@@ -90,13 +147,17 @@ function processAnswer(question) {
         }
 
         else if (answer.type == "image") {
-            addAnswerOutput(ANSWER_IMAGE_FORMAT(imagePath + answer.content));
+            addAnswerOutput(ANSWER_IMAGE_FORMAT(IMAGE_PATH + answer.content));
         }
 
         else if (answer.type == "sql") {
 
+            addAnswerOutput("⌛ Antwoord aan het ophalen...")
+
+            // Connect to backend server to retrieve which gets the answer from database
             $.get(DB_URL, {question: answer.content}, (data, status) => {
                 if (status == "success") {
+                    clearAnswers();
                     addAnswerOutput(ANSWER_TEXT_FORMAT(data));
                 }
 
@@ -109,37 +170,61 @@ function processAnswer(question) {
 
 }
 
+/**
+ * Send a chat message
+ * @param sender The name of the sender
+ * @param message The message tos end
+ */
 function sendMessage(sender, message) {
     MESSAGE_BOX.innerHTML = MESSAGE(sender, message) + MESSAGE_BOX.innerHTML;
 }
 
+/**
+ * Add an answer to the answer display output
+ * @param output The output to display
+ */
 function addAnswerOutput(output) {
     ANSWER_OUTPUT.innerHTML += output;
 }
 
+/**
+ * Clear answer output, including the shown question
+ */
 function clearOutput() {
     QUESTION_DISPLAY.innerHTML = "";
+    clearAnswers();
+}
+
+/**
+ * Clear only the answers part of the output
+ */
+function clearAnswers() {
     ANSWER_OUTPUT.innerHTML = "";
 }
 
-
+/**
+ * Show the list of questions
+ */
 function showHelp() {
     sendMessage(BOT, "Dit zijn de vragen die ik kan beantwoorden:");
 
-    const divider = "--------------------------------------";
-    sendMessage(BOT, divider);
-
     QUESTIONS.forEach((question) => {
-        sendMessage(BOT, "- " + question.question);
+        sendMessage(BOT, QUESTION_FORMAT(question.question));
     });
-
-    sendMessage(BOT, divider);
 }
 
+/**
+ * Clear the input field of the message box
+ */
 function clearInputField() {
     INPUT_FIELD.value = "";
 }
 
+/**
+ * Cleans up a question for processing
+ * @param question The question
+ * @returns {string} The sanitized question
+ */
 const sanitizedQuestion = (question) => {
     let sanitized = question.toLowerCase();
 
@@ -149,6 +234,11 @@ const sanitizedQuestion = (question) => {
     return sanitized;
 };
 
+/**
+ * Check of the input contains a greeting
+ * @param input The input to analyze
+ * @returns {boolean} True if it contains a greeting, false if it does not
+ */
 function containsGreeting(input) {
     const greetings = ["hey", "hallo", "hello", "hoi", "goedemiddag", "goededag"];
     return greetings.includes(input);
--- js/questions.js
+++ js/questions.js
@@ -4,7 +4,7 @@ class Question {
      * Creates a question
      * @param question The full question
      * @param keywords The keywords to match the question
-     * @param answers An array of answers to this question, which can be text, an image, or multiple of both
+     * @param answers An array of answers to this question, which can be text, an image, sql, or multiple types
      */
     constructor(question, keywords, answers) {
         this.question = question;
@@ -43,11 +43,6 @@ class Answer {
     }
 }
 
-/*
-Each question has:
-question: The full question that will be shown on screen
-keywords: An array of keywords
- */
 
 /**
  * QUESTIONS is an array of Question objects.
@@ -58,6 +53,13 @@ keywords: An array of keywords
  * @type {Question[]}
  */
 export const QUESTIONS = [
+
+    new Question("Welke factoren leiden tot een successvolle film?",
+        [ "welke", "factoren", "leiden", "tot", "succesvolle", "film"],
+        [ new Answer("text", "Er is geprobeerd een logistisch regressiemodel te maken. Helaas kwam daaruit dat" +
+            " een succesvolle film niet goed te voorspellen valt met alleen data. Hieronder is de ROC curve van dat model:"),
+            new Answer("image", "factoren_succesvolle_film.jpg"),]
+    ),
 
     new Question("Welk filmgenre heeft gemiddeld de hoogste rating?",
         ["genre", "filmgenre", "rating", "hoogste", "beste", "beoordeling"],
@@ -91,7 +93,7 @@ export const QUESTIONS = [
     ),
 
     new Question("Is er een verband tussen het jaartal en het budget van films?",
-        ["is", "er", "verband", "tussen", "jaar", "jaartal", "budget", "kosten", "film", "films"],
+        ["verband", "tussen", "jaar", "jaartal", "budget", "kosten", "film", "films"],
         [
             new Answer("text", "Er is een lineair regressiemodel gemaakt met R. De R² was minder dan 0.01 en correlatie was 0.05. Er is geen verband." +
             " Hieronder is een plot. Het lijkt alsof recentelijk films duurder zijn geworden, maar in deze grafiek zijn dat maar een klein aantal van vele duizende films" +
@@ -104,7 +106,16 @@ export const QUESTIONS = [
     new Question("Hoe ziet een visuele weergave van het budget van een film gerelateerd aan het aantal acteurs eruit?",
         ["hoe", "ziet", "er", "uit", "eruit", "visuele", "weergave", "grafiek", "plot", "budget", "film", "gerelateerd", "aan",  "aantal", "acteurs", "eruit"],
         [new Answer("image", "cost_and_actors.jpg")]
+    ),
+
+    new Question("Is er een verband tussen het budget en de lengte van een film?",
+        ["verband", "tussen", "budget", "correlatie", "lengte", "film"],
+        [ new Answer("text", "In de grafiek lijkt er niet duidelijk een verband te zijn. Doormiddel" +
+        " van een statistische toets is echter bepaald dat  er toch wel een correlatie is. Echter niet een heel sterk verband."),
+        new Answer("image", "cost_and_time.jpg")]
     )
+
+
 ];
 
 
